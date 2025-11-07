c40ef4289cb05bd285265aa69078729a701a0eb9
ppaniewski <p.paniewski@gmail.com>
2025-11-07 18:02:53 +0100

    Make changes for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import cookieParser from 'cookie-parser';
 import connectMongoDB from './config/dbConnect.js';
 import startAgenda from './agenda/index.js';
@@ -8,12 +10,17 @@ import userRoutes from './routes/userRoutes.js';
 import portfolioRoutes from './routes/portfolioRoutes.js';
 import stockRoutes from './routes/stockRoutes.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const clientDist = path.join(__dirname, "../../frontend/dist");
+
 const app = express();
 
 dotenv.config();
 await connectMongoDB();
 
 // Middleware
+app.use(express.static(clientDist));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
@@ -22,6 +29,12 @@ app.use(cookieParser());
 app.use("/api/users", userRoutes);
 app.use("/api/portfolio", portfolioRoutes);
 app.use("/api/stocks", stockRoutes);
+
+// SPA fallback
+app.use((req, res, next) => {
+    if (req.path.startsWith("/api")) return next();
+    res.sendFile(path.join(clientDist, "index.html"));
+});
 
 app.use(errorHandler);
 
